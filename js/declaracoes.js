5254ee002578e5eb2792fa9d67dbd9fcbd779ef3
NicolasFune <nicolas290@live.com>
2021-11-05 18:18:05 -0300

    05/11/2021

--- js/declaracoes.js
+++ js/declaracoes.js
@@ -6,7 +6,7 @@ let sec_01 = document.getElementById("sec_01")
 let headerOpacity = document.getElementById('header').style.opacity
 let mouse__interaction__areas_des = document.getElementsByClassName('mouse__interaction__area_des')
 let leftPositionSlide = 0
-
+let index = 0
 let mouse__interaction__areas_aud = document.getElementsByClassName('mouse__interaction__area_aud')
 
 
@@ -25,9 +25,23 @@ let bot_des_txt = document.getElementsByClassName('call__button__text__des').ite
 let bot_aud_txt = document.getElementsByClassName('call__button__text__aud').item(0)
 let bot_tec_txt = document.getElementsByClassName('call__button__text__tec').item(0)
 
-let carrosel_des = document.getElementsByClassName('carrosel_des__').item(0)
-let carrosel_aud = document.getElementsByClassName('carrosel_aud__').item(0)
-let carrosel_tec = document.getElementsByClassName('carrosel_tec__').item(0)
+let carrosel_des = document.getElementsByClassName('carrosel_des__')
+let carrosel_aud = document.getElementsByClassName('carrosel_aud__')
+let carrosel_tec = document.getElementsByClassName('carrosel_tec__')
 
 let prevSlide__des = document.getElementById('prevSlide--button--des')
 let nextSlide__des = document.getElementById('nextSlide--button--des')
+let prevSlide__aud = document.getElementById('prevSlide--button--aud')
+let nextSlide__aud = document.getElementById('nextSlide--button--aud')
+let prevSlide__tec = document.getElementById('prevSlide--button--tec')
+let nextSlide__tec = document.getElementById('nextSlide--button--tec')
+
+let dot_01_des = document.getElementById('dot_01_des')
+let dot_02_des = document.getElementById('dot_02_des')
+let dot_03_des = document.getElementById('dot_03_des')
+let dot_01_aud = document.getElementById('dot_01_aud')
+let dot_02_aud = document.getElementById('dot_02_aud')
+let dot_03_aud = document.getElementById('dot_03_aud')
+let dot_01_tec = document.getElementById('dot_01_tec')
+let dot_02_tec = document.getElementById('dot_02_tec')
+let dot_03_tec = document.getElementById('dot_03_tec')
--- js/funcoes.js
+++ js/funcoes.js
@@ -1,28 +1,293 @@
-prevSlide__des.addEventListener('click',prevSlide__Click)
-let prevSlide__Click = function (){
+prevSlide__des.addEventListener('click',function VoltarSlide(){
+    
     leftPositionSlide = leftPositionSlide + 33.33
-    carrosel_des.style.left = leftPositionSlide + 'vw'
-    alert('prev')
-}
-
-nextSlide__des.addEventListener('click',nextSlide__Click)
-
-let nextSlide__Click =  function (){
+    if(leftPositionSlide > 0){
+        leftPositionSlide = -66.66
+    }
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_des.style.backgroundColor = 'white'
+            dot_02_des.style.backgroundColor = 'white'
+            dot_03_des.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_des.style.backgroundColor = 'white'
+            dot_02_des.style.backgroundColor = 'black'
+            dot_03_des.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_des.style.backgroundColor = 'black'
+            dot_02_des.style.backgroundColor = 'white'
+            dot_03_des.style.backgroundColor = 'white'
+            break;
+    }
+})
+nextSlide__des.addEventListener('click',function AvançarSlide(){
     leftPositionSlide = leftPositionSlide - 33.33
-    carrosel_des.style.left = leftPositionSlide + 'vw'
-}
-
-
+    if(leftPositionSlide < -66.66){
+        leftPositionSlide = 0
+    }
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_des.style.backgroundColor = 'white'
+            dot_02_des.style.backgroundColor = 'white'
+            dot_03_des.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_des.style.backgroundColor = 'white'
+            dot_02_des.style.backgroundColor = 'black'
+            dot_03_des.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_des.style.backgroundColor = 'black'
+            dot_02_des.style.backgroundColor = 'white'
+            dot_03_des.style.backgroundColor = 'white'
+            break;
+    }
+})
+prevSlide__aud.addEventListener('click',function VoltarSlide(){
+    
+    leftPositionSlide = leftPositionSlide + 33.33
+    if(leftPositionSlide > 0){
+        leftPositionSlide = -66.66
+    }
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_aud.style.backgroundColor = 'white'
+            dot_02_aud.style.backgroundColor = 'white'
+            dot_03_aud.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_aud.style.backgroundColor = 'white'
+            dot_02_aud.style.backgroundColor = 'black'
+            dot_03_aud.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_aud.style.backgroundColor = 'black'
+            dot_02_aud.style.backgroundColor = 'white'
+            dot_03_aud.style.backgroundColor = 'white'
+            break;
+    }
+})
+nextSlide__aud.addEventListener('click',function AvançarSlide(){
+    leftPositionSlide = leftPositionSlide - 33.33
+    if(leftPositionSlide < -66.66){
+        leftPositionSlide = 0
+    }
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_aud.style.backgroundColor = 'white'
+            dot_02_aud.style.backgroundColor = 'white'
+            dot_03_aud.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_aud.style.backgroundColor = 'white'
+            dot_02_aud.style.backgroundColor = 'black'
+            dot_03_aud.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_aud.style.backgroundColor = 'black'
+            dot_02_aud.style.backgroundColor = 'white'
+            dot_03_aud.style.backgroundColor = 'white'
+            break;
+    }
+})
+prevSlide__tec.addEventListener('click',function VoltarSlide(){
+    
+    leftPositionSlide = leftPositionSlide + 33.33
+    if(leftPositionSlide > 0){
+        leftPositionSlide = -66.66
+    }
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_tec.style.backgroundColor = 'white'
+            dot_02_tec.style.backgroundColor = 'white'
+            dot_03_tec.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_tec.style.backgroundColor = 'white'
+            dot_02_tec.style.backgroundColor = 'black'
+            dot_03_tec.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_tec.style.backgroundColor = 'black'
+            dot_02_tec.style.backgroundColor = 'white'
+            dot_03_tec.style.backgroundColor = 'white'
+            break;
+    }
+})
+nextSlide__tec.addEventListener('click',function AvançarSlide(){
+    leftPositionSlide = leftPositionSlide - 33.33
+    if(leftPositionSlide < -66.66){
+        leftPositionSlide = 0
+    }
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.left = leftPositionSlide + 'vw'
+        index = index + 1
+    })
+    index=0
+    switch (leftPositionSlide) {
+        case -66.66:
+            dot_01_tec.style.backgroundColor = 'white'
+            dot_02_tec.style.backgroundColor = 'white'
+            dot_03_tec.style.backgroundColor = 'black'
+            break;
+        case -33.33:
+            dot_01_tec.style.backgroundColor = 'white'
+            dot_02_tec.style.backgroundColor = 'black'
+            dot_03_tec.style.backgroundColor = 'white'
+            break;
+        default:
+            dot_01_tec.style.backgroundColor = 'black'
+            dot_02_tec.style.backgroundColor = 'white'
+            dot_03_tec.style.backgroundColor = 'white'
+            break;
+    }
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+dot_01_des.addEventListener('click',function SelecionarSlide01() {
+    dot_01_des.style.backgroundColor = 'black'
+    dot_02_des.style.backgroundColor = 'white'
+    dot_03_des.style.backgroundColor = 'white'
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.left= '0vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_02_des.addEventListener('click',function SelecionarSlide02() {
+    dot_01_des.style.backgroundColor = 'white'
+    dot_02_des.style.backgroundColor = 'black'
+    dot_03_des.style.backgroundColor = 'white'
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.left= '-33.33vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_03_des.addEventListener('click',function SelecionarSlide03() {
+    dot_01_des.style.backgroundColor = 'white'
+    dot_02_des.style.backgroundColor = 'white'
+    dot_03_des.style.backgroundColor = 'black'
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.left= '-66.66vw'
+        index = index + 1
+    })
+    index=0
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+dot_01_aud.addEventListener('click',function SelecionarSlide01() {
+    dot_01_aud.style.backgroundColor = 'black'
+    dot_02_aud.style.backgroundColor = 'white'
+    dot_03_aud.style.backgroundColor = 'white'
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.left= '0vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_02_aud.addEventListener('click',function SelecionarSlide02() {
+    dot_01_aud.style.backgroundColor = 'white'
+    dot_02_aud.style.backgroundColor = 'black'
+    dot_03_aud.style.backgroundColor = 'white'
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.left= '-33.33vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_03_aud.addEventListener('click',function SelecionarSlide03() {
+    dot_01_aud.style.backgroundColor = 'white'
+    dot_02_aud.style.backgroundColor = 'white'
+    dot_03_aud.style.backgroundColor = 'black'
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.left= '-66.66vw'
+        index = index + 1
+    })
+    index=0
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+dot_01_tec.addEventListener('click',function SelecionarSlide01() {
+    dot_01_tec.style.backgroundColor = 'black'
+    dot_02_tec.style.backgroundColor = 'white'
+    dot_03_tec.style.backgroundColor = 'white'
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.left= '0vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_02_tec.addEventListener('click',function SelecionarSlide02() {
+    dot_01_tec.style.backgroundColor = 'white'
+    dot_02_tec.style.backgroundColor = 'black'
+    dot_03_tec.style.backgroundColor = 'white'
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.left= '-33.33vw'
+        index = index + 1
+    })
+    index=0
+})
+dot_03_tec.addEventListener('click',function SelecionarSlide03() {
+    dot_01_tec.style.backgroundColor = 'white'
+    dot_02_tec.style.backgroundColor = 'white'
+    dot_03_tec.style.backgroundColor = 'black'
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.left= '-66.66vw'
+        index = index + 1
+    })
+    index=0
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 let TornarControlesVisiveis_des = function(){
     for(let i=0;i<carrosel_controls_des.length;i++){
         carrosel_controls_des[i].style.opacity = "1"
         
     }
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.animation = 'none'
+        carrosel_des.item(index).left = '0vw'
+        index = index + 1
+    })
+    index=0
 }
 let TornarControlesInvisiveis_des = function(){
     for(let i=0;i<carrosel_controls_des.length;i++){
         carrosel_controls_des[i].style.opacity = "0"
     }
+    Array.from(carrosel_des).forEach(function name(params) {
+        carrosel_des.item(index).style.animation = 'slide_img 10s infinite ease-in'
+        index = index + 1
+    })
+    index=0
 }
 Array.from(mouse__interaction__areas_des).forEach(function name(item) {
     item.addEventListener('mouseover', TornarControlesVisiveis_des)
@@ -30,16 +295,27 @@ Array.from(mouse__interaction__areas_des).forEach(function name(item) {
 Array.from(mouse__interaction__areas_des).forEach(function name(item) {
     item.addEventListener('mouseout', TornarControlesInvisiveis_des)
 })
-//////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let TornarControlesVisiveis_aud = function(){
     for(let i=0;i<carrosel_controls_aud.length;i++){
         carrosel_controls_aud[i].style.opacity = "1"
     }
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.animation = 'none'
+        carrosel_aud.item(index).left = '0vw'
+        index = index + 1
+    })
+    index=0
 }
 let TornarControlesInvisiveis_aud = function(){
     for(let i=0;i<carrosel_controls_aud.length;i++){
         carrosel_controls_aud[i].style.opacity = "0"
     }
+    Array.from(carrosel_aud).forEach(function name(params) {
+        carrosel_aud.item(index).style.animation = 'slide_img 10s infinite ease-in'
+        index = index + 1
+    })
+    index=0
 }
 Array.from(mouse__interaction__areas_aud).forEach(function name(item) {
     item.addEventListener('mouseover', TornarControlesVisiveis_aud)
@@ -47,16 +323,27 @@ Array.from(mouse__interaction__areas_aud).forEach(function name(item) {
 Array.from(mouse__interaction__areas_aud).forEach(function name(item) {
     item.addEventListener('mouseout', TornarControlesInvisiveis_aud)
 })
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let TornarControlesVisiveis_tec = function(){
     for(let i=0;i<carrosel_controls_tec.length;i++){
         carrosel_controls_tec[i].style.opacity = "1"
     }
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.animation = 'none'
+        carrosel_tec.item(index).left = '0vw'
+        index = index + 1
+    })
+    index=0
 }
 let TornarControlesInvisiveis_tec = function(){
     for(let i=0;i<carrosel_controls_tec.length;i++){
         carrosel_controls_tec[i].style.opacity = "0"
     }
+    Array.from(carrosel_tec).forEach(function name(params) {
+        carrosel_tec.item(index).style.animation = 'slide_img 10s infinite ease-in'
+        index = index + 1
+    })
+    index=0
 }
 Array.from(mouse__interaction__areas_tec).forEach(function name(item) {
     item.addEventListener('mouseover', TornarControlesVisiveis_tec)
@@ -64,6 +351,8 @@ Array.from(mouse__interaction__areas_tec).forEach(function name(item) {
 Array.from(mouse__interaction__areas_tec).forEach(function name(item) {
     item.addEventListener('mouseout', TornarControlesInvisiveis_tec)
 })
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 window.addEventListener('resize', function resize() {
     let videoHeight = video.clientHeight
     sec_01.style.height = videoHeight+"px"
@@ -72,8 +361,8 @@ window.addEventListener('resize', function resize() {
     let videoHeight = video.clientHeight
     sec_01.style.height = videoHeight+"px"
 }, false);
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 function bot_menu_click(){
     if(bot_clicado==false){
         menu.style.visibility = "visible"
